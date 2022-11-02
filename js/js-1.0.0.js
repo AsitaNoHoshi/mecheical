a43bba9b2546630462c449b9a6ef1f9c49d308f1
AsitaNoHoshi <assoonas111@gmail.com>
2022-11-02 23:10:42 +0800

    1.0.0.1

---
+++ js/js-1.0.0.js
@@ -0,0 +1,15 @@
+function test(e) {
+    // 取得選取選項的文字
+    let check_text = e.target.innerText;
+    // 抓取切換箱
+    let toggle_menu = document.querySelectorAll('ul.toggle-menu>li');
+
+    toggle_menu.forEach(
+        function(items) {
+            items.style.display = 'none';
+        }
+    );
+    if (check_text == '升學資訊') toggle_menu[0].style.display = 'block';
+    else if (check_text == '大學端校網') toggle_menu[1].style.display = 'block';
+    else toggle_menu[2].style.display = 'block';
+}
