8f6d68bc53b13aeb915f0d0fecef1d0465cccd32
AsitaNoHoshi <assoonas111@gmail.com>
2022-11-09 15:18:06 +0800

    js
    js改寫

--- js/js-1.0.0.js
+++ js/js-1.0.0.js
@@ -1,15 +1,15 @@
-function test(e) {
-    // 取得選取選項的文字
-    let check_text = e.target.innerText;
+function test(bartext) {
     // 抓取切換箱
     let toggle_menu = document.querySelectorAll('ul.toggle-menu>li');
-
     toggle_menu.forEach(
         function(items) {
             items.style.display = 'none';
         }
     );
-    if (check_text == '升學資訊') toggle_menu[0].style.display = 'block';
-    else if (check_text == '大學端校網') toggle_menu[1].style.display = 'block';
+    if (bartext == '升學資訊') toggle_menu[0].style.display = 'block';
+    else if (bartext == '大學端校網') toggle_menu[1].style.display = 'block';
     else toggle_menu[2].style.display = 'block';
 }
+let varr = document.querySelectorAll('.toggle-bar');
+// e.path[e.path.indexOf(document)].title 判斷來源網頁
+if(varr.length != 0) varr[0].addEventListener('click',function(e) {test(e.target.innerText);},false);
